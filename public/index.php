5ed35b0ef1a629aed48b2aefe26f5f3ed933f928
Ben Schöffmann benedikt.schoeffmann@gmail.com <benedikt.schoeffmann@gmail.com>
2020-05-22 06:54:00 +0200

    Add: public dir with basic index

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?
+
+
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
